2660f35f37a955df82d89fdd9aaa77820e285851
Misbah Aamer <misbahuddin.aamer@gmail.com>
2021-03-20 18:34:03 -0400

    init

---
+++ Play.Catalog.Service/DTOS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Catalog.Service
+{
+    public record ItemDTO
+    (
+        Guid Id,
+        string Name,
+        string Description,
+        decimal Price,
+        DateTimeOffset CreatedDate
+    );
+
+    public record CreateItemDTO
+    (
+        string Name,
+        string Description,
+        decimal Price
+    );
+
+    public record UpdateItemDTO
+    (
+        string Name,
+        string Description,
+        decimal Price
+    );
+
+}
+
